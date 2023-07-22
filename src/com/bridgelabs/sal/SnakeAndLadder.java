8f8a66dbec651ed878476bfa63174462d27a3059
admin <aasthabarve0@gmail.com>
2023-07-22 17:39:35 +0530

    UC3 Added

--- src/com/bridgelabs/sal/SnakeAndLadder.java
+++ src/com/bridgelabs/sal/SnakeAndLadder.java
@@ -5,8 +5,31 @@ public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("Snake and Ladder Game");
         System.out.println("Initial position : " + playerPosition);
+
         int diceValue = (int)(Math.random()*10)%6+1;
         System.out.println("Dice Value = " + diceValue);
+
+        int option = (int)(Math.random()*10%3);
+        System.out.println("Choice = " + option);
+
+        switch (option) {
+            case 0:
+                System.out.println("no play");
+                break;
+            case 1:
+                //for ladder
+                playerPosition = playerPosition + diceValue;
+                System.out.println(playerPosition);
+                break;
+            case 2:
+                //for snake
+                playerPosition = playerPosition - diceValue;
+                System.out.println(playerPosition);
+                break;
+            default:
+                System.out.println("Invalid");
+                break;
+        }
     }
 }
 
